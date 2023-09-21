4115550601d48ee16bde4d44abd5a3544e9a8a4a
Danil <d_silchenkov@mail.ru>
2023-09-21 20:53:19 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите натуральное число n > 99 = ");
+int n = int.Parse(Console.ReadLine());
+int n1 = n / 10;
+int n2 = (n / 100) % 10;
+Console.WriteLine($"Десятков в числе {n} = {n1}");
+Console.WriteLine($"Сотен в числе {n} = {n2}");
